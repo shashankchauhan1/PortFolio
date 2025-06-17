4848ad5cc8841a30514e11d50db9eeadbde6182a
shashank <shashank1049.be23@chitkara.edu.in>
2025-06-17 22:58:07 +0530

    in

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  base: '/portfolio/', 
-})
+  base: '/PortFolio/',
+  plugins: [react()],
+});
